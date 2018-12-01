4d1d1447baed7ab566ee135ac6a5668f3f53ff75
Ahmad El-Baher <ahmad.elbaher@gmail.com>
2018-11-30 17:19:47 -0800

    Changed MESSAGE.WIN to MESSAGE.BID_OVER

--- auctionsystem/client.py
+++ auctionsystem/client.py
@@ -201,7 +201,7 @@ class AuctionClient:
         # Item has been sold to another client
         self.bidding_ended(item_num)
         if self.gui_update_cb:
-            self.gui_update_cb(MESSAGE.WIN, item_num, amount)
+            self.gui_update_cb(MESSAGE.BID_OVER, item_num, amount)
         else:
             print("You are NOT the winner of item {}, bought for {}!".format(item_num, amount))
 
